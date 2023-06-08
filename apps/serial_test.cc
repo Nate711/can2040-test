84ebe10d145aec46a43d5eda9c72f3226340d472
Nathan Kau <nathankau@gmamil.com>
2023-06-08 23:20:16 +0900

    test using ftdi. result: bad, 10ms max latency

--- apps/serial_test.cc
+++ apps/serial_test.cc
@@ -10,9 +10,11 @@
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
+#include "pupperv3/utils.h"
 
-constexpr int kToggleLEDTime = 500'000;  // us
-constexpr int kPrintTime = 1000;         // us
+constexpr int kBaudRate = 1'000'000;
+constexpr double kSendRate = 1000.0;  // Hz
+
 static char message[50] = "012345670123456701234567012345670123456701234567\n";
 
 constexpr int UART_TX_PIN = 16;
@@ -25,29 +27,27 @@ int main(void) {
   int32_t ledState = 0;
   stdio_init_all();
 
-  uart_init(uart0, 115200);
+  uart_init(uart0, kBaudRate);
   gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
   gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
-  int count = 0;
+  int sent_messages_count = 0;
 
-  uint64_t last_led_toggle = time_us_64();
-  uint64_t last_msg_sent = time_us_64();
+  utils::PeriodicCaller led_toggler(2.0);
+  utils::PeriodicCaller send_message_timer(kSendRate);
   while (1) {
-    if (time_us_64() - last_led_toggle > kToggleLEDTime) {
-      last_led_toggle = time_us_64();
+    led_toggler.tick([&]() {
       gpio_put(LED_PIN, ledState);
       if (ledState == 0) {
         ledState = 1;
       } else {
         ledState = 0;
       }
-    }
-    if (time_us_64() - last_msg_sent > kPrintTime) {
-      last_msg_sent = time_us_64();
+    });
+    send_message_timer.tick([&]() {
       printf(message);
       stdio_flush();
-      count++;
-    }
+      sent_messages_count++;
+    });
   }
 }
